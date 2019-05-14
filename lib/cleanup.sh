ffbe0d7f339a80cfde88728139240d7b1c65dc82
jsecchiero <secchierojacopo@gmail.com>
2019-05-14 19:15:28 +0200

    use standard formatting for logging

--- lib/cleanup.sh
+++ lib/cleanup.sh
@@ -23,29 +23,29 @@ node_exist() {
 }
 
 while [ "$(node_exist)" ]; do
-  echo deregister node from consul
-  curl -sS -X PUT "http://$${consul}:$${consul_port}/v1/agent/force-leave/$${identity}-$${_NUMBER}"
+  echo "$(date +%x\ %H:%M:%S) Deregister node from consul"
+  curl -sS -X PUT "http://$${consul}:$${consul_port}/v1/agent/force-leave/$${identity}-$${_NUMBER}" > /dev/null
   curl -sS -X PUT "http://$${consul}:$${consul_port}/v1/catalog/deregister?dc=$${consul_datacenter}" --data \{\"Datacenter\":\"$${consul_datacenter}\",\"Node\":\"$${identity}-$${_NUMBER}\"\} > /dev/null
   sleep 1
 done
 
 if [ "$quantity" = "0" ]; then
-  echo "is the last node so clean up everything"
+  echo "$(date +%x\ %H:%M:%S) Is the last node so clean up everything"
   # Remove consul keys
-  curl -sS -X DELETE "http://$${consul}:$${consul_port}/v1/kv/$${cluster_name}/$${identity}?recurse=yes"
+  curl -sS -X DELETE "http://$${consul}:$${consul_port}/v1/kv/$${cluster_name}/$${identity}?recurse=yes" > /dev/null
 
   # Cleanup all cluster if last group
   cluster_group_kv=$(curl -H "Accept: application/json" -Ss http://$${consul}:$${consul_port}/v1/kv/$${cluster_name}?recurse=yes)
   if [ "$?" != "0" ]; then
-    echo curl error in cluster_group_kv
+    echo "$(date +%x\ %H:%M:%S) Curl error in cluster_group_kv"
     exit 1
   fi
 
   cluster_group_exists=$(echo $${cluster_group_kv} | jq '.[].Key' | egrep ".*/.*/.*" | wc -l)
   if [ "$cluster_group_exists" = "0" ]; then
-    echo "is the last group so clean up everything"
+    echo "$(date +%x\ %H:%M:%S) Is the last group so clean up everything"
     # Remove cluster consul keys
-    curl -sS -X DELETE "http://$${consul}:$${consul_port}/v1/kv/$${cluster_name}?recurse=yes"
+    curl -sS -X DELETE "http://$${consul}:$${consul_port}/v1/kv/$${cluster_name}?recurse=yes" > /dev/null
   fi
 fi
 
