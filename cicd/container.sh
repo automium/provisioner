5f9fe16f2d2cfe9e1db07c21526f3e65de8e0961
jsecchiero <secchierojacopo@gmail.com>
2021-03-18 12:15:05 +0100

    Try to avoid docker hub rate limit

--- cicd/container.sh
+++ cicd/container.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 IMAGE_NAME=automium/service-provisioner
-docker build --no-cache --pull --tag "$IMAGE_NAME" .
 docker login -u "$REGISTRY_USER" -p "$REGISTRY_PASS"
+docker build --no-cache --pull --tag "$IMAGE_NAME" .
 docker tag "$IMAGE_NAME" "${IMAGE_NAME}:${TRAVIS_BRANCH}"
 docker tag "$IMAGE_NAME" "${IMAGE_NAME}:latest"
 docker push "${IMAGE_NAME}:${TRAVIS_BRANCH}"
